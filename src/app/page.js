fc61f9652e100a0cf2248a0488ce87f9dc8bd72d
FAH <apichaya.inkhiewsai@gmail.com>
2023-08-28 15:59:10 +0700

    fin

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,58 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Footer } from "@/components/Footer";
+import {
+  Button,
+  Container,
+  Divider,
+  Group,
+  Pagination,
+  Rating,
+  Text,
+  TextInput,
+  Textarea,
+  Title,
+} from "@mantine/core";
 
 export default function FoodReviewPage() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
-
-      <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+      <Title order={4} mt="sm">
+        Your rating
+      </Title>
+      <Rating size="lg" />
+      <Textarea
+        mt="xs"
+        label="Your review"
+        placeholder="Do you enjoy eating?"
+        minRows={3}
+      ></Textarea>
+      <Button mt="xs" color="orange">
+        Submit Review
+      </Button>
+      <Divider my="sm" />
+      <Group position="center">
+        {" "}
+        <Title order={4}>Elon Musk</Title> <Rating value={5} readOnly></Rating>
+      </Group>
+      <Text align="center" c="gray">
+        Best pizza in this world. I give you X score.
       </Text>
+      <Divider my="sm" />
+      <Group position="center">
+        {" "}
+        <Title order={4}>Mark Zuck</Title> <Rating value={4} readOnly></Rating>
+      </Group>
+      <Text align="center" c="gray">
+        My favourite part is pepperoni
+      </Text>
+      <Pagination
+        total={20}
+        position="center"
+        color="orange"
+        mt="md"
+      ></Pagination>
+      <Footer></Footer>
     </Container>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import { Text } from "@mantine/core";
+
 export const Footer = () => {
   return (
-    <div>
-      <p>Copyright ©</p>
-    </div>
+    <Text align="center" color="dimmed" my="sm">
+      Copyright © 2023 Apichaya Inkhiewsai 650610820
+    </Text>
   );
 };
